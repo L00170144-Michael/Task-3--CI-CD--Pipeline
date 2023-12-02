2aaa38aabe5964898670abc5d7d6c8a123c33687
Michael <L00170144@atu.ie>
2023-12-02 21:53:30 +0000

    adding my spring boot project created from Sping Initializr

---
+++ Task3/src/main/java/com/atu/l00170144/Task3/Task3Application.java
@@ -0,0 +1,13 @@
+package com.atu.l00170144.Task3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task3Application.class, args);
+	}
+
+}
