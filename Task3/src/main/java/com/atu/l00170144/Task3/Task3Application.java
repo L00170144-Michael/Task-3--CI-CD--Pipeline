8f783c53ca67239a169d723a9f6ff4fc19b54daa
Michael <L00170144@atu.ie>
2023-12-02 22:57:02 +0000

    added a simple getmapping

--- Task3/src/main/java/com/atu/l00170144/Task3/Task3Application.java
+++ Task3/src/main/java/com/atu/l00170144/Task3/Task3Application.java
@@ -2,12 +2,19 @@ package com.atu.l00170144.Task3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Task3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Task3Application.class, args);
 	}
-
+	
+	@GetMapping
+	public String Hello() {
+		return "Hello World";
+	}
 }
